4c792f551bc18f71a34356d0934dd083c25275cc
HubertSia <hubemail2021@gmail.com>
2025-01-22 23:38:33 -0500

    Commented on the task 6

--- TASK6/js/script.js
+++ TASK6/js/script.js
@@ -1,28 +1,52 @@
 "use strict";
 
 function setup() {
+  // Set up canvas 640X640
   createCanvas(400, 400); 
+  
+  // Background black
   background(0); 
 
-  const textString = "test"; 
+  // Variable text for "test"
+  const textString = "test";
+  
+  // Variable for the text size of 28px
   const textSizeValue = 28;
+  
+  //Variable of centering the axis
   const centerX = width / 2; 
   const centerY = height / 2; 
 
+  // Color white
   fill(255); 
-  textSize(textSizeValue); 
+  
+  // the text size value
+  textSize(textSizeValue);
+  
+  // Text alignment
   textAlign(CENTER, CENTER); 
+  
+  // Show the text
   text(textString, centerX, centerY); 
 
 
+  // Loop the number 10 times
   for (let i = 0; i < 10; i++) {
+    
+    // Loop the numbers on the X axis
     const xPosition = 10 + i * 15; 
+    
+    // Show the text as numbers
     text(i, xPosition, 20); 
   }
 
-
+  // Loop the number 15 times
   for (let i = 1; i <= 15; i++) {
+    
+      // Loop the numbers on the Y axis
     const yPosition = 20 + i * 20; 
+    
+    // Show the text as numbers
     text(i, 10, yPosition); 
   }
   
