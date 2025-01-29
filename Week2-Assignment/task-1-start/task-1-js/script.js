94d98f8996a47bfc12961f3730673fed2524132f
HubertSia <hubemail2021@gmail.com>
2025-01-28 22:01:53 -0500

    Part 2 completed

--- Week2-Assignment/task-1-start/task-1-js/script.js
+++ Week2-Assignment/task-1-start/task-1-js/script.js
@@ -100,37 +100,60 @@ console.log(lastImage);
  */
 
 /*************************************** */
-/*** END PART ONE ACCESS */ 
+/*** END PART ONE ACCESS */
 
 
 /*************************************** */
-/*** START PART TWO MODIFY */ 
+/*** START PART TWO MODIFY */
 /*************************************** */
 /* 1: Select the first paragraph and replace the text within the paragraph... */
+
 /***CODE */
+ document.querySelector("p").innerHTML = "<p>text changed by ``Hubert Sia`` on the following date: `28 January`.</p>";
+  
+    
 /*************************************** */
 /* 2: Select all elements in the HTML that have the class name content-container
  and change the background color ... of first and second ...*/
-/***CODE */
+
+    /***CODE */
+document.querySelectorAll(".content-container")[0].style.backgroundColor = "orange";
+document.querySelectorAll(".content-container")[1].style.backgroundColor = "purple";
+    
 
 /*************************************** */
 /* 3: Change the src element of the first image element on the page to be ...
 /***CODE */
+document.querySelector(".img-image").src = "task-1-images/seven.png";
 
 /*************************************** */
 /* 4: Select the third paragraph element on the page and 
 replace the content (within the paragraph) to be an h2 element which contains the text `TEST 123`
+
 /***CODE */
+document.querySelectorAll("p")[2].innerHTML = "<h2>TEST 123</h2>";
+
 
 /*************************************** */
 /* 5: Select the fourth paragraph element on the page and 
 add to the existing content an h2 element containing the text `TEST 123`
+
 /***CODE */
+document.querySelectorAll("p")[3].innerHTML = "<h2>TEST 123</h2>";
 
 /*************************************** */
 /* 6: Select the fifth paragraph element on the page and add to the existing content 
 an img element that holds `one.png`, and add the class newStyle to said paragraph element.
+
 /***CODE */
+let fifthParagraph = document.querySelectorAll("p")[4]; 
+fifthParagraph.classList.add("newStyle"); 
+
+let newImage = document.createElement("img");
+newImage.src = "task-1-images/one.png"; 
+newImage.alt = "Image one"; 
+
+fifthParagraph.appendChild(newImage);
 
 
 /*************************************** */
@@ -140,8 +163,16 @@ Next, iterate over the colors array, and for each color:
 assign the element from innerContainers variable with the same index 
 (i.e. colors[0] should be allocated to the first innerContainers element, colors[1] to the second, etc ...) 
 a background using that color.
-/***CODE */
 
+/***CODE */
+let colors = ['red', 'blue', 'green', 'orange'];
+let innerContainers = document.querySelectorAll(".inner-container");
+
+for (let i = 0; i < colors.length; i++) {
+    innerContainers[i].style.backgroundColor = colors[i];
+}
+
+    
 /*************************************** */
 /*** END PART TWO MODIFY */ 
 
